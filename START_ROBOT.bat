c24d4ccd29c3746f6d8bf60b34b222c0f37ad61e
ferdinandmanampiring-netizen <ferdinand.manampiring@gmail.com>
2026-04-13 08:20:04 +0700

    Update START_ROBOT.bat

--- START_ROBOT.bat
+++ START_ROBOT.bat
@@ -1,1 +1,13 @@
 
+@echo off
+title SNIPER POWERFUL FERDY - LIVE CONSOLE
+color 0A
+taskkill /f /im python.exe >nul 2>&1
+echo --------------------------------------------------
+echo [SYSTEM] Membersihkan sisa sesi lama...
+del sniper_session.session-journal >nul 2>&1
+echo [SYSTEM] Memulai Sniper Powerful Ferdy di Jendela Baru...
+echo --------------------------------------------------
+:: Menjalankan di jendela baru agar tidak mengganggu Cursor
+start powershell.exe -NoExit -Command "python 05_main_orchestrator.py"
+exit
